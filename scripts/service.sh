34fae60eb43a7d873d44f6c56c9e9fd1069572a5
kevdog824 <kjerome64@gmail.com>
2025-02-15 22:41:36 -0500

    - Fix shell script for mac - Removed hardcoded cd from batch file - Made restart/update command work on Mac

--- scripts/service.sh
+++ scripts/service.sh
@@ -1,2 +1,2 @@
-.venv\bin\python3 -m uvicorn mkhome:app --host 0.0.0.0 --port 8582 --log-config config/logging.yaml --log-level info
+.venv/bin/python3 -m uvicorn mkhome:app --host 0.0.0.0 --port 8582 --log-config config/logging.yaml --log-level info
  
--- src/mkhome/routes/app.py
+++ src/mkhome/routes/app.py
@@ -1,9 +1,10 @@
 import logging
 import os
 import subprocess
-import sys
+import threading
+import time
 
-from fastapi import APIRouter, HTTPException
+from fastapi import BackgroundTasks, APIRouter, HTTPException
 
 
 LOGGER = logging.getLogger(__name__)
@@ -11,25 +12,42 @@ ROUTER = APIRouter(prefix="/app")
 
 
 def restart() -> None:
+    cmd = [
+        ".venv/bin/python3",
+        "-m",
+        "uvicorn",
+        "mkhome:app",
+        "--host",
+        "0.0.0.0",
+        "--port",
+        "8582",
+        "--log-config",
+        "config/logging.yaml",
+        "--log-level",
+        "info",
+    ]
+    os.execvp(cmd[0], cmd)
+
+
+@ROUTER.get("/update", status_code=202, tags=["Application"])
+async def update_application(background_tasks: BackgroundTasks) -> None:
     try:
-        python = sys.executable
-        os.execl(python, python, *sys.argv)
+        subprocess.run(["git", "checkout", "main"])
+        time.sleep(1)
+        subprocess.run(["git", "pull", "origin", "main"])
+        time.sleep(1)
+        background_tasks.add_task(restart)
     except Exception:
-        raise HTTPException(500, "Failed to restart application")
+        LOGGER.exception("Failed to update application")
+        raise HTTPException(500, "Internal service error")
 
 
-@ROUTER.get("/update", tags=["Application"])
-async def update_application() -> None:
-    subprocess.run("git checkout main")
-    subprocess.run("git pull origin/main")
-    restart()
-
-
-@ROUTER.get("/restart", tags=["Application"])
+@ROUTER.get("/restart", status_code=202, tags=["Application"])
 async def restart_application() -> None:
     """Restarts this application"""
     try:
-        restart()
+        t = threading.Thread(target=restart, daemon=False)
+        t.start()
     except Exception:
         LOGGER.exception("Failed to restart application")
         raise HTTPException(500, "Internal service error")
